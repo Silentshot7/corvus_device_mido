89f7b257541862e8148bfe491d6305337f0d34da
NRanjan-17 <nalinishranjan05@gmail.com>
2022-05-25 15:37:36 +0530

    mido: Changes for Corvus OS
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_mido.mk
+    $(LOCAL_DIR)/corvus_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_mido-user \
-    evolution_mido-userdebug \
-    evolution_mido-eng
+    corvus_mido-user \
+    corvus_mido-userdebug \
+    corvus_mido-eng
--- corvus_mido.mk
+++ corvus_mido.mk
@@ -22,18 +22,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common evolution stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Custom stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_MINI_GAPPS := true
+RAVEN_LAIR := Unofficial
+CORVUS_MAINTAINER := Silentshot7
+USE_GAPPS=true
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := evolution_mido
+PRODUCT_NAME := corvus_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
